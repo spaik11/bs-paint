38c09e0c819d926ba87ef121f9fc117e361634eb
Sung Paik <sung.paik@codeimmersives.com>
2019-11-20 23:41:17 -0500

    included event listeners to the palette colors. trying to change the current brush

--- main.js
+++ main.js
@@ -4,25 +4,34 @@ function grabColor(element) {
     element.classList[1];
 }
 
+const squareColors = document.querySelectorAll('.square');
+
+for (let i = 0; i < squareColors.length; i++) {
+    squareColors[i] = squareColors[i].addEventListener('click', clickSquare);
+}
+
+const square = document.querySelector('.square');
+
 function clickSquare(event) {
-    const squareColors = document.querySelectorAll('.square');
-
-    for (let i = 0; i < squareColors.length; i++) {
-        squareColors[i] = squareColors[i].addEventListener('click', clickSquare);
-    }
-
     event.target.classList.replace(grabColor(square), grabColor(currentBrush));
 }
 
-function clickPalette() {
-    const paletteColors = document.querySelectorAll('.palette-color');
+const paletteColors = document.querySelectorAll('.palette-color')
 
-    for (let i = 0; i < paletteColors.length; i++) {
-        paletteColors[i] = paletteColors[i].addEventListener('click', clickPalette)
-    }
+paletteColors[0].addEventListener('click', clickPalette)
+paletteColors[1].addEventListener('click', clickPalette)
+paletteColors[2].addEventListener('click', clickPalette)
+paletteColors[3].addEventListener('click', clickPalette)
+paletteColors[4].addEventListener('click', clickPalette)
 
-    currentBrush.classList.replace(grabColor(currentBrush), grabColor())
+// for (let i = 0; i < paletteColors.length; i++) {
+//     paletteColors[i] = paletteColors[i].addEventListener('click', clickPalette);
+// }
+
+function clickPalette(event) {
+    currentBrush.classList.replace(grabColor(currentBrush), grabColor(paletteColors[0]))
 }
+
 
 
 
