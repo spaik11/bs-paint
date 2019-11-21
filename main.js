c1c61b44bd7d64e0b136788b53f0d2553233468a
Sung Paik <sung.paik@codeimmersives.com>
2019-11-20 22:10:45 -0500

    included code to loop through each class to add event listeners. still need to work on the functionality of picking the colors

--- main.js
+++ main.js
@@ -5,15 +5,25 @@ function grabColor(element) {
 }
 
 function clickSquare(event) {
+    const squareColors = document.querySelectorAll('.square');
+
+    for (let i = 0; i < squareColors.length; i++) {
+        squareColors[i] = squareColors[i].addEventListener('click', clickSquare);
+    }
+
     event.target.classList.replace(grabColor(square), grabColor(currentBrush));
 }
 
 function clickPalette() {
-    currentBrush.classList.replace(grabColor(currentBrush), grabColor());
+    const paletteColors = document.querySelectorAll('.palette-color');
+
+    for (let i = 0; i < paletteColors.length; i++) {
+        paletteColors[i] = paletteColors[i].addEventListener('click', clickPalette)
+    }
+
+    currentBrush.classList.replace(grabColor(currentBrush), grabColor())
 }
 
-const paletteColors = document.querySelectorAll('.palette-color');
 
-for (let i = 0; i < paletteColors.length; i++) {
-    paletteColors[i] = paletteColors[i].addEventListener('click', )
-}
+
+
